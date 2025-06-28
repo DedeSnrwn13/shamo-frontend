15ab923d433d4d897dfedf3bdc3f829becbca207
DedeSnrwn13 <sunarwandede13@gmail.com>
2025-06-28 18:38:55 +0700

    feat: home page header

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -4,16 +4,57 @@ import 'package:shamo_frontend/theme.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  Widget header() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(
+        defaultMargin,
+        defaultMargin,
+        defaultMargin,
+        defaultMargin,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hallo, Alex',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 24,
+                    fontWeight: semiBold,
+                  ),
+                ),
+                Text(
+                  '@alexkeinn',
+                  style: subtitleTextStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: 54,
+            height: 54,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage('assets/image_profile.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home Page',
-        style: primaryTextStyle.copyWith(
-          fontSize: 20,
-          fontWeight: medium,
-        ),
-      ),
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
