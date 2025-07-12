95895c35514e0c18014c05e4931fd671c6da0541
DedeSnrwn13 <sunarwandede13@gmail.com>
2025-07-12 12:00:11 +0700

    feat: profile page header

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -6,14 +6,58 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Profile Page',
-        style: primaryTextStyle.copyWith(
-          fontSize: 20,
-          fontWeight: medium,
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(defaultMargin),
+            child: Row(
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    'assets/image_profile.png',
+                    width: 64,
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hallo, Alex',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 24,
+                          fontWeight: semiBold,
+                        ),
+                      ),
+                      Text(
+                        '@alexander',
+                        style: subtitleTextStyle.copyWith(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Image.asset(
+                  'assets/button_exit.png',
+                  width: 20,
+                ),
+              ],
+            ),
+          ),
         ),
-      ),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+      ],
     );
   }
 }
