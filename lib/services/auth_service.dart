fcb7ab41020d633084b04faa7eba9452221ba638
DedeSnrwn13 <sunarwandede13@gmail.com>
2025-08-09 11:12:05 +0700

    feat: add auth service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:shamo_frontend/models/user_model.dart';
+
+class AuthService {
+  static const String baseUrl = 'http://50.0.43.45:8000/api';
+
+  Future<UserModel> register({
+    required String name,
+    required String username,
+    required String email,
+    required String password,
+  }) async {
+    var url = '$baseUrl/register';
+    var headers = {'Content-Type': 'application/json'};
+    var body = jsonEncode({
+      'name': name,
+      'username': username,
+      'email': email,
+      'password': password,
+    });
+
+    var response = await http.post(
+      Uri.parse(url),
+      headers: headers,
+      body: body,
+    );
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body)['data'];
+      UserModel user = UserModel.fromJson(data['user']);
+      user.token = 'Bearer ${data['access_token']}';
+
+      return user;
+    } else {
+      throw Exception('Register Failed');
+    }
+  }
+}
